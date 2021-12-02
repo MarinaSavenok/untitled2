c2f60370ee28ff3448c86d2dc0e41ed11b7eb7d4
Marina Savenok <Savenok-marina@mail.ru>
2021-12-02 20:43:33 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int total = initialBalance + remittance;
+        System.out.println(total);
+    }
+}
